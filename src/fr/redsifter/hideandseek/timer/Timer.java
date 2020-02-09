a998a1eb596c1935b0cc3fa6800e852e1144f789
Nathan Hallouin <nathan.hallouin@gmx.fr>
2020-02-09 19:08:01 +0100

    suppression du commentaire de test

--- src/fr/redsifter/hideandseek/timer/Timer.java
+++ src/fr/redsifter/hideandseek/timer/Timer.java
@@ -3,9 +3,7 @@ package fr.redsifter.hideandseek.timer;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class Timer extends BukkitRunnable {
-	
-	//commentaire timer
-	
+
 	private int timer;
 	@Override
 	public void run() {

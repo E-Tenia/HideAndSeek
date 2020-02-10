e1beb3ba613037a00371a9d893e21f3269827f18
3MPTYB0Y <EmptyB0Y@users.noreply.github.com>
2020-02-10 14:22:35 +0100

    Creation de teams fonctionnelle

--- src/fr/redsifter/hideandseek/commands/Commands.java
+++ src/fr/redsifter/hideandseek/commands/Commands.java
@@ -11,14 +11,34 @@ import org.bukkit.scoreboard.Team;
 
 public class Commands implements CommandExecutor {
 	
+	private String[] purge(String[] list) {
+		
+		String[] gm = new String[list.length];
+		int x = 0;
+		for(int b = 0; b < list.length;b++) {
+			if (list[b] != null) {
+				gm[x] = list[b];
+				x++;
+			}
+		}
+		System.out.println(x);
+		String[] gmlst = new String[x];
+		for(int a = 0; a < gmlst.length;a++) {
+			if (gm[a] != null) {
+				gmlst[a] = gm[a];
+			}
+		}
+
+		return gmlst;
+	}
+	
 	private void createTeam(String nm,String[] gm) {
-		String name = nm;
 		Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
 
 		Team team = null;
 
 		for (Team t : scoreboard.getTeams()) {
-			if (t.getName().equals(name)) {
+			if (t.getName().equals(nm)) {
 				team = t;
 				break;
 			}
@@ -27,6 +47,7 @@ public class Commands implements CommandExecutor {
 		if (team == null)
 			team = scoreboard.registerNewTeam(nm);
 		if (nm == "hide") {
+			System.out.println("Hiders created");
 			team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
 		}
 		for(String str : gm) {
@@ -52,29 +73,48 @@ public class Commands implements CommandExecutor {
 				int i = 0;
 				int j = 0;
 				int a = 0;
+				int b = 0;
+				int x = 0;
 				String[] gamelist = new String[12];
 				for (i = 1;i < args.length;i++){
 					gamelist[j] = args[i];
 					j++;
 					}
-				String[] hiders = new String[6];
-				String[] seekers = new String[6];
+				String[] purged = purge(gamelist);
+				int init1 = 0;
+				int init2 = 0;
+				if (purged.length %2 == 0) {
+					init1 = purged.length/2;
+					init2 = purged.length/2;
+					}
+				else {
+					init1 = purged.length-1;
+					init2 = purged.length+1;
+				}
+				if (init1 == 0 || init2 == 0) {
+					sender.sendMessage("Precise players to invite");
+					break;
+				}
+				String[] hiders = new String[init2];
+				String[] seekers = new String[init1];
 				Location spawn = new Location(Bukkit.getWorld("world"),(-223.500),66,(-213.500));
 				for(j = 0;j < i-1;j++) {
 					Player pl = Bukkit.getPlayerExact(gamelist[j]);
 					if (pl != null) {
-						if (j - a > (i/2)-1) {
+						if (j - x > (i/2)-1) {
 							sender.sendMessage("Inviting " + gamelist[j] + " as Hider !");
-							hiders[j - a] = gamelist[j];
+							hiders[b - x] = gamelist[j];
+							b++;
 						}
 						else{
 							sender.sendMessage("Inviting " + gamelist[j] + " as Seeker !");
-							seekers[j - a] = gamelist[j];
+							seekers[a - x] = gamelist[j];
+							a++;
 						}
 						pl.teleport(spawn);
 					}
 					else {
-						a++;
+						x++;
 						sender.sendMessage("Player " + gamelist[j] + " is not here !");
 					}
 				}
